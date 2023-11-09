7e1f97e99d4951943c67f54a6a3cae9b135284c1
th-release <cth.release@gmail.com>
2023-11-09 20:31:50 +0900

    feat: Image upload

--- src/model/model.controller.ts
+++ src/model/model.controller.ts
@@ -1,16 +1,52 @@
 import { Router } from "express";
 import ModelService from "./model.service";
+import MulterUtils from "../utils/multerUtils";
+import multer from "multer";
+import path from "path";
+import jsonwebtoken from "../utils/jsonwebtoken";
+import Mongo from "../utils/mongoose";
+import authModel from "../entities/auth.entity";
 
 export class ModelController {
   private router: Router = Router();
   private readonly modelService: ModelService = new ModelService();
+  private readonly Multer: multer.Multer;
 
   constructor(prefix: string) {
+    this.Multer = new MulterUtils({
+      cbFunction: async (req, file, callback) => {
+        const token = req.headers.authorization;
+        const verify = jsonwebtoken.verify(token, process.env.JWT_SECRET);
+        const user = await new Mongo().findOne(authModel, {
+          _id: verify.uuid
+        })
+
+        if (!token || !verify.success || !user)
+          return callback(new Error('비 정상적인 토큰 입니다.'), 'public/trash');
+        else {
+          return callback(null, `public/tmp/${user.username}/tp`);
+        }
+      },
+      filter: async (req, file, callback) => {
+        const token = req.headers.authorization;
+        const verify = jsonwebtoken.verify(token, process.env.JWT_SECRET);
+        const user = await new Mongo().findOne(authModel, {
+          _id: verify.uuid
+        })
+
+        if (!token || !verify.success || !user)
+          return callback(null, false)
+        else {
+          return callback(null, true)
+        }
+      }
+    }, ['image/png', 'image/jpeg'], 120).getMulter();
+
     this.initializeRoutes(prefix);
   }
 
   private initializeRoutes(prefix: string): void {
-    this.router.post(`${prefix}/fileUpload`, (req, res) => this.modelService.fileUpload(req, res))
+    this.router.post(`${prefix}/upload`, this.Multer.array('file', 30), (req, res) => this.modelService.fileUpload(req, res))
   }
 
   public getRouter(): Router {
--- src/model/model.service.ts
+++ src/model/model.service.ts
@@ -3,6 +3,10 @@ import Mongo from "../utils/mongoose";
 
 export default class ModelService {
   public async fileUpload(req: Request, res: Response) {
+    console.log(req.files)
 
+    return res.status(201).json({
+      success: true
+    });
   }
 }
